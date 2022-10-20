9e4aacc6f01548d055d6396e15ace1e9326035cb
Paul Koreng <p.koreng@heimrich-hannot.de>
2022-10-20 09:46:50 +0200

    use PageAssetTrait

--- src/Asset/FrontendAssets.php
+++ src/Asset/FrontendAssets.php
@@ -1,51 +1,34 @@
 <?php
 
 /*
- * Copyright (c) 2021 Heimrich & Hannot GmbH
+ * Copyright (c) 2022 Heimrich & Hannot GmbH
  *
  * @license LGPL-3.0-or-later
  */
 
 namespace HeimrichHannot\TinySliderBundle\Asset;
 
-use HeimrichHannot\UtilsBundle\Container\ContainerUtil;
-use Symfony\Component\DependencyInjection\ContainerInterface;
+use HeimrichHannot\EncoreContracts\PageAssetsTrait;
+use Symfony\Contracts\Service\ServiceSubscriberInterface;
 
-class FrontendAssets
+class FrontendAssets implements ServiceSubscriberInterface
 {
-    /**
-     * @var ContainerInterface
-     */
-    private $container;
-    /**
-     * @var ContainerUtil
-     */
-    private $containerUtil;
-
-    /**
-     * FrontendAssets constructor.
-     */
-    public function __construct(ContainerInterface $container, ContainerUtil $containerUtil)
-    {
-        $this->container = $container;
-        $this->containerUtil = $containerUtil;
-    }
+    use PageAssetsTrait;
 
     /**
      * Setup the frontend assets needed for tiny slider bundle.
      */
     public function addFrontendAssets()
     {
-        if ($this->containerUtil->isFrontend()) {
-            if ($this->container->has('huh.encore.asset.frontend')) {
-                $this->container->get('huh.encore.asset.frontend')->addActiveEntrypoint('contao-tiny-slider-bundle');
-                $this->container->get('huh.encore.asset.frontend')->addActiveEntrypoint('contao-tiny-slider-bundle-theme');
-            }
-
-            $GLOBALS['TL_CSS']['tiny-slider'] = 'bundles/contaotinyslider/tiny-slider.css';
-            $GLOBALS['TL_JAVASCRIPT']['tiny-slider'] = 'bundles/contaotinyslider/tiny-slider.js';
-            $GLOBALS['TL_CSS']['contao-tiny-slider-bundle'] = 'bundles/contaotinyslider/contao-tiny-slider-bundle-theme.css';
-            $GLOBALS['TL_JAVASCRIPT']['contao-tiny-slider-bundle'] = 'bundles/contaotinyslider/contao-tiny-slider-bundle.js';
-        }
+        $this->addPageEntrypoint('contao-tiny-slider-bundle', [
+            'TL_CSS' => [
+                'tiny-slider' => 'bundles/contaotinyslider/tiny-slider.css',
+                'contao-tiny-slider-bundle' => 'bundles/contaotinyslider/contao-tiny-slider-bundle-theme.css',
+            ],
+            'TL_JAVASCRIPT' => [
+                'tiny-slider' => 'bundles/contaotinyslider/tiny-slider.js',
+                'contao-tiny-slider-bundle' => 'bundles/contaotinyslider/contao-tiny-slider-bundle.js',
+            ],
+        ]);
     }
 }
